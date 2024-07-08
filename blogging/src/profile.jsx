0ec0083018904378f84034c938a68e026bf4e502
Sameer <nenusaitam.sameer@gmail.com>
2024-07-09 02:35:24 +0530

    Profile Update

--- blogging/src/profile.jsx
+++ blogging/src/profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./profile.css";
 import { auth } from "./google/config";
+import BlogsCard from "./blogsCard";
 
 function Profile({ user }) {
   const signOut = () => {
@@ -8,12 +9,20 @@ function Profile({ user }) {
   };
 
   return (
-    <div className="profile-main">
-      <img src={user.photoURL} alt="Profile Pic" className="profile-pic" />
-      <h3 className="profile-name">{user.displayName}</h3>
-      <p className="profile-email">{user.email}</p>
-      <button onClick={signOut}>Sign Out</button>
-    </div>
+    <>
+      <div className="profile-main">
+        <div className="profile-first">
+          <img src={user.photoURL} alt="Profile Pic" className="profile-pic" />
+          <h3 className="profile-name">{user.displayName}</h3>
+          <p className="profile-email">{user.email}</p>
+          <div className="profile-dash"></div>
+        </div>
+        <div className="profile-second">
+          <h2>Your Articles :</h2>
+          <BlogsCard />
+        </div>
+      </div>
+    </>
   );
 }
 
