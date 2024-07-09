ffe843d11bf0621c92344582f29f07acd47c2b59
Sameer <nenusaitam.sameer@gmail.com>
2024-07-09 21:31:41 +0530

    Profile Update

---
+++ blogging/src/editArticle.jsx
@@ -0,0 +1,11 @@
+import "./editArticle.css";
+
+function EditArticle() {
+  return (
+    <>
+      <h1>Edit Article</h1>
+    </>
+  );
+}
+
+export default EditArticle;
---
+++ blogging/src/uploadArticle.jsx
@@ -0,0 +1,11 @@
+import "./uploadArticle.css";
+
+function UploadArticle() {
+  return (
+    <>
+      <h1>Upload Article</h1>
+    </>
+  );
+}
+
+export default UploadArticle;
